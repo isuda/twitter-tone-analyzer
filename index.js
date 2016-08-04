6db19195ce8679bbd035badf054ac6448476ccf5
Ian Suda <ian@ayogo.com>
2016-08-04 16:20:14 -0700

    Add express to serve tone via api.
    - add timestamp to results.

--- index.js
+++ index.js
@@ -4,27 +4,36 @@ var config = require('./config');
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 var Twitter = require('twitter');
 var Promise = require('bluebird');
+var express = require('express');
 
 var toneAnalyzer = new ToneAnalyzerV3(config.auth.watson.tone_analyzer);
-
 var client = new Twitter(config.auth.twitter);
+var app = express();
+
 
 var getTweets = Promise.promisify(client.get, {context: client});
 var getTone   = Promise.promisify(toneAnalyzer.tone, {context: toneAnalyzer});
 
-getTweets('search/tweets', {q: 'cascadia16'})
-  .then(function(tweets) {
-    return tweets.statuses.reduce(function(p, c) {
-      p.push(c.text);
-      return p;
-    }, []);
-  })
-  .then(function(tweets) {
-    return Promise.all(tweets.map(function(t) {
-      return getTone({text: t});
-    }));
-  })
-  .then(function(toneResults) {
-    console.log(toneResults);
-    console.log(toneResults.length);
-  });
+app.use('/', express.static(__dirname + '/public'));
+app.get('/tone/:hashtag', function (req, res) {
+  getTweets('search/tweets', {q: req.params.hashtag})
+    .then(function(tweets) {
+      return Promise.all(tweets.statuses.map(function(tweet, i) {
+        return getTone({text: tweet.text})
+                .then(function(tone) {
+                  return {
+                    text: tweet.text,
+                    time: tweet.created_at,
+                    tone: tone
+                  };
+                });
+      }));
+    })
+    .then(function(toneResults) {
+      res.json({results: toneResults});
+    });
+});
+
+app.listen(config.port, function () {
+  console.log('Express app listening on port '+config.port+'!');
+});
